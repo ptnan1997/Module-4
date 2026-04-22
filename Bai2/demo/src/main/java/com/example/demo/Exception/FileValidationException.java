6f1f2e5e3332376d0f478570eaf5ea2ee892b56d
ptnan1997 <ptnan1997@gmail.com>
2026-04-23 08:22:43 +0900

    Delete Bai2/demo directory

--- Bai2/demo/src/main/java/com/example/demo/Exception/FileValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Exception;
-
-public class FileValidationException extends RuntimeException {
-    public FileValidationException(String message) {
-        super(message);
-    }
-}
--- Bai2/demo/src/main/java/com/example/demo/repository/DepartmentRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepos extends JpaRepository<Department,Long> {
-}

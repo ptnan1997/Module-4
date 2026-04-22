49540b95ed5f2ced693b2125b68c988bdbb7e256
ptnan1997 <ptnan1997@gmail.com>
2026-04-23 08:26:59 +0900

    Add files via upload

---
+++ Session9/Bai3/demo/src/main/java/com/example/demo/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.demo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        ApiResponse<?> response = ApiResponse.builder()
+                .status("FAIL")
+                .message("Validation failed")
+                .data(errors)
+                .build();
+        return new  ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
